88177cf8034e65dfd17fe629d942b0061a733ad4
Anshul Chaturvedi <anshul.chaturvedi1012@gmail.com>
2025-08-26 08:51:20 +0530

    Day-11

---
+++ Day-011 Maths in DSA - I/EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class EvenOdd{
+	static boolean isEven(int num){
+		if(num%2==0) return true;
+		else return false;
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number:  ");
+		int num = sc.nextInt();
+		
+		if(isEven(num)) System.out.println("The number is even");
+		else System.out.println("The number is odd");
+	}
+}
